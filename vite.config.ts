78dfae69a42dd72bebb25f35f853e68b177f80cb
Alan-J-Bibins <alanjbibins@gmail.com>
2026-07-01 23:01:50 +0530

    refactor: Format config and initial format

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,21 @@
-import { defineConfig } from "vite-plus";
+import { defineConfig } from 'vite-plus';
 
 export default defineConfig({
-  staged: {
-    "*": "vp check --fix",
-  },
-  fmt: {},
-  lint: {
-    jsPlugins: [{ name: "vite-plus", specifier: "vite-plus/oxlint-plugin" }],
-    rules: { "vite-plus/prefer-vite-plus-imports": "error" },
-    options: { typeAware: true, typeCheck: true },
-  },
-  run: {
-    cache: true,
-  },
+    staged: {
+        '*': 'vp check --fix',
+    },
+    fmt: {
+        singleQuote: true,
+        sortImports: true,
+        semi: true,
+        tabWidth: 4,
+    },
+    lint: {
+        jsPlugins: [{ name: 'vite-plus', specifier: 'vite-plus/oxlint-plugin' }],
+        rules: { 'vite-plus/prefer-vite-plus-imports': 'error' },
+        options: { typeAware: true, typeCheck: true },
+    },
+    run: {
+        cache: true,
+    },
 });
